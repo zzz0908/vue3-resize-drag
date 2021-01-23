0008e051f20ed1262d5262c59a07470663c1e6c2
zzz0908 <984643994@qq.com>
2021-01-23 13:52:12 +0800

    修改

---
+++ src/vue3-resize-drag.ts
@@ -0,0 +1,15 @@
+import vue3ResizeDrag from '@/components/vue3-resize-drag.vue'
+const vue3ResizeDragObj = {  //  定义一个对象
+    install (Vue:any) {  // 需要拥有一个 install 方法
+        Vue.component(vue3ResizeDrag.name, vue3ResizeDrag)  // vuePayKeyboard.name 组件的name属性
+        // 类似通过 this.$xxx 方式调用插件的 其实只是挂载到原型上而已
+        // Vue.prototype.$xxx  // 最终可以在任何地方通过 this.$xxx 调用
+        // 虽然没有明确规定用$开头  但是大家都默认遵守这个规定
+        // 新增
+        if (typeof window !== 'undefined' && Vue) {
+            Vue.use(vue3ResizeDrag);
+        }
+    }
+}
+// 导出这个对象
+export default vue3ResizeDragObj
