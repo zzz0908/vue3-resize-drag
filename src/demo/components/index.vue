a2f2bc0dbfaf88562b5c968667b073d3fbb35646
cotx-zhangxudong <you@example.com>
2021-01-26 10:35:04 +0800

    键盘事件

--- src/demo/components/index.vue
+++ src/demo/components/index.vue
@@ -1,6 +1,6 @@
 <template>
     <vue3ResizeDrag 
-    v-for="(item,index) in data" 
+    v-for="(item,index) in data.data" 
     :key="item.name" 
     :x="item.x" 
     :y="item.y" 
@@ -34,62 +34,64 @@ export default defineComponent({
     //   vue3ResizeDrag
   },
   setup() {
-      const data = reactive([
-          {
-              x:100,
-              y:100,
-              name:'哈哈',
-              drag:false,
-              resize:true,
-              rotate:true,
-              active:false
-          },
-          {
-              x:200,
-              y:200,
-              name:'哈哈2',
-              drag:true,
-              resize:true,
-              rotate:true,
-              active:true
-          },
-          {
-              x:300,
-              y:300,
-              name:'哈哈3',
-              drag:true,
-              resize:true,
-              rotate:true,
-              active:false
-          },
-          {
-              x:400,
-              y:400,
-              name:'哈哈4',
-              drag:true,
-              resize:true,
-              rotate:true,
-              active:false
-          },
-          {
-              x:500,
-              y:500,
-              name:'哈哈5',
-              drag:true,
-              resize:true,
-              rotate:true,
-              active:false
-          },
-          {
-              x:600,
-              y:600,
-              name:'哈哈6',
-              drag:true,
-              resize:false,
-              rotate:false,
-              active:false
-          }
-      ])
+      let data = reactive({
+          data:[
+            {
+                x:100,
+                y:100,
+                name:'哈哈',
+                drag:false,
+                resize:true,
+                rotate:true,
+                active:false
+            },
+            {
+                x:200,
+                y:200,
+                name:'哈哈2',
+                drag:true,
+                resize:true,
+                rotate:true,
+                active:true
+            },
+            {
+                x:300,
+                y:300,
+                name:'哈哈3',
+                drag:true,
+                resize:true,
+                rotate:true,
+                active:false
+            },
+            {
+                x:400,
+                y:400,
+                name:'哈哈4',
+                drag:true,
+                resize:true,
+                rotate:true,
+                active:false
+            },
+            {
+                x:500,
+                y:500,
+                name:'哈哈5',
+                drag:true,
+                resize:true,
+                rotate:true,
+                active:false
+            },
+            {
+                x:600,
+                y:600,
+                name:'哈哈6',
+                drag:true,
+                resize:false,
+                rotate:false,
+                active:false
+            }
+        ]
+      })
       const moveHandler = (data:object) => {
           console.log(data,'移动')
       }
@@ -115,7 +117,7 @@ export default defineComponent({
       //   自制激活事件 点击组件激活
       const activeMouseDown = (item:any,index: number) => {
           if(currentIndex.value === index) return
-          data.forEach((item,i) => {
+          data.data.forEach((item,i) => {
               if(i === index){
                 item['active'] = true
               }else{
@@ -123,6 +125,48 @@ export default defineComponent({
               }
           })
           currentIndex.value = index
+      }
+      let copyData = reactive({
+          copy:{}
+      })
+      document.onkeydown = function(e:any){  // 绑定键盘事件
+        e = e || window.event;  //标准化事件处理
+        if (e.keyCode == 46) {  // delete  按键盘delete删除
+            data.data = data.data.filter((item,index) => index != currentIndex.value)
+        } else if (e.ctrlKey && e.keyCode == 67) {  // Ctrl + C  // 复制
+            if(currentIndex.value >= 0){
+                copyData.copy = Object.assign({},data.data[currentIndex.value])
+            }
+        } else if (e.ctrlKey && e.keyCode == 86) {   // Ctrl + V  // 粘贴
+            let copy: object = Object.assign({},copyData.copy)
+            let copyReactive = reactive({
+                copy
+            })
+            console.log(copyReactive.copy)
+            // data.data.push(copyReactive.copy)
+        } else if (e.ctrlKey && e.keyCode == 90) {  // Ctrl + Z  // 撤回
+            //   暂时没写
+        } else if (e.ctrlKey) {
+            //   暂时没写
+        }else if(e.keyCode == 37 || e.keyCode == 38 || e.keyCode == 39 || e.keyCode == 40){  // 键盘上下左右移动激活中的组件
+              //      左                  上                  右                下
+              if(currentIndex.value >= 0){
+                switch (e.keyCode) {
+                    case 37:
+                        data.data[currentIndex.value].x -= 1
+                        break;
+                      case 38:
+                          data.data[currentIndex.value].y --
+                          break;
+                      case 39:
+                          data.data[currentIndex.value].x ++
+                          break;
+                      case 40:
+                          data.data[currentIndex.value].y ++
+                          break;
+                }
+              }
+        }
       }
       return {
           data,
